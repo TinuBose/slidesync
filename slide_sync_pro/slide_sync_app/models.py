5007d458ad181c1b500ba5b664a5dbcd708bf50b
tinu bose <tinubose.mca2022@gmail.com>
2024-03-23 11:43:43 +0530

    created and deployed machine learning model for converting pdf to png

--- slide_sync_pro/slide_sync_app/models.py
+++ slide_sync_pro/slide_sync_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class brands:
+    name : str
+    available : bool
--- slide_sync_pro/slide_sync_app/urls.py
+++ slide_sync_pro/slide_sync_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('proceed', views.proceed,name='proceed')
+    path('proceed', views.proceed,name='proceed'),
+    path('present', views.present,name='present')
 ]
